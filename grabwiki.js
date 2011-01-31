cee99fd226f7d19b3705e6f964eee83eae4b4c9b
Atul Varma <varmaa@gmail.com>
2011-01-31 17:56:13 +0000

    Updated grabwiki.js to work with node.js 0.3.6.

--- grabwiki.js
+++ grabwiki.js
@@ -3,32 +3,31 @@
 // content for any Wiki page on wiki.mozilla.org.
 
 var http = require('http');
+var https = require('https');
 const WIKI_DOMAIN = 'wiki.mozilla.org';
 
 http.createServer(function(req, res) {
-  var wiki = http.createClient(443, WIKI_DOMAIN, true);
   var parts = require('url').parse(req.url);
   var title = encodeURIComponent(parts.pathname.slice(1));
 
-  wiki.on('error', function(ex) {
-    console.log("Error when fetching " + req.url);
-    console.log(ex.stack);
-  });
-
-  var wikiReq = wiki.request('GET', '/index.php?title=' + title +
-                             '&action=render',
-                             {'host': WIKI_DOMAIN});
-  wikiReq.end();
-  wikiReq.on('response', function(wikiRes) {
+  var wikiReq = https.get({
+    host: WIKI_DOMAIN,
+    path: '/index.php?title=' + title + '&action=render'
+  }, function(wikiRes) {
     res.writeHead(wikiRes.statusCode, {
       'Content-Type': 'text/html; charset=utf-8',
-      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Origin': '*'
     });
     wikiRes.on('data', function(chunk) {
       res.write(chunk);
     });
     wikiRes.on('end', function() {
       res.end();
-    });
+    });    
+  });
+
+  wikiReq.on('error', function(ex) {
+    console.log("Error when fetching " + req.url);
+    console.log(ex.stack);
   });
 }).listen(8291);
